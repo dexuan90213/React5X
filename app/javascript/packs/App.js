5ab062824c3dfdad40cd5b22995696f5f6736946
Eden Yang <dexuan90213@gmail.com>
2019-12-20 16:57:35 +0800

    add react App.js

---
+++ app/javascript/packs/App.js
@@ -0,0 +1,44 @@
+import React, {useState} from 'react'
+import ReactDOM from 'react-dom'
+
+const clickH1 = function() { alert('h1 clicked!') }
+
+const LangItem = ({lang}) => <li>{lang}</li>
+
+const LangList = ({langs}) => {
+  return (<ul>
+    {langs.map(l => <LangItem lang={l} />)}
+  </ul>)
+}
+
+const LangInput = ({addLang}) => {
+  let [inputText, setInputText] = useState("")
+  return (
+    <>
+      <input type="text"
+             value={inputText}
+             onChange={function(evt) { setInputText(evt.target.value) }} />
+      <button onClick={ () => {
+        addLang(inputText)
+        setInputText("")
+      }}>
+        Don't click!
+      </button>
+    </>
+  )
+}
+
+const App = () => {
+  let [langs, setLangs] = useState(['react', 'ruby', 'elixir', 'haskell'])
+
+  return (
+    <div>
+      <h1 onClick={clickH1}> Hello from React!</h1>
+      <p>hehe</p>
+      <LangList langs={langs}/>
+      <LangInput addLang={l => setLangs([...langs, l])} />
+    </div>
+    )
+  }
+
+ReactDOM.render(<App />, document.querySelector('#container'))
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,10 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+$(document).ready(function () {
+  let time = moment().add(1, 'days').calendar();
+  console.log(time)
+})
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
